e39b544e9b61f66ad7de5802a92e4b4b7f026ddf
Hao <hdeng88@users.noreply.github.com>
2020-04-01 10:41:32 -0400

    update
    add space

--- R/pformat.r
+++ R/pformat.r
@@ -6,6 +6,6 @@
 pformat = function(x) {
   out1 = ifelse(is.na(x),yes = NA,
          ifelse(is.numeric(x) == F,NA,
-         ifelse(x<0.001,"< 0.001",ifelse(x>=1,">0.999",x))))
+         ifelse(x<0.001,"< 0.001",ifelse(x>=1,"> 0.999",x))))
   return(out1)
   }
